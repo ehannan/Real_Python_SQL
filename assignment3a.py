fcc77329706bfb82bd4d20cf313b879eea53aeb6
Emily Hannan <genjgal@gmail.com>
2015-10-03 23:16:39 -0500

    assignment updated

--- assignment3a.py
+++ assignment3a.py
@@ -1,1 +1,29 @@
-__author__ = 'emily'
+# Database containing 100 random integers ranging from 0 to 100
+
+
+# import the sql library
+import sqlite3
+import random
+
+# create the database, if it doesn't already exist
+conn = sqlite3.connect("newnum.db")
+
+with sqlite3.connect("newnum.db") as connection:
+    c = connection.cursor()
+
+    # delete database table if exist
+    c.execute("DROP TABLE if exists numbers")
+
+    # create database table
+    c.execute("CREATE TABLE numbers(num int)")
+
+    # insert each number to the database
+    for i in range(100):
+        c.execute("INSERT INTO numbers VALUES(?)",(random.randint(0,100),))
+
+    c.execute("SELECT * FROM numbers")
+
+    rows = c.fetchall()
+
+    for r in rows:
+        print r[0]
--- assignment3b.py
+++ assignment3b.py
@@ -1,1 +1,41 @@
-__author__ = 'emily'
+# Assignment 3b - return aggregation from prompt
+
+
+import sqlite3
+
+with sqlite3.connect("newnum.db") as connection:
+    c = connection.cursor()
+
+    average = ("SELECT avg(num) FROM  numbers")
+    maximum = ("SELECT max(num) FROM numbers")
+    minimum = ("SELECT min(num) FROM numbers")
+    sum = ("SELECT sum(num) FROM numbers")
+
+    while True:
+        prompt = raw_input("Enter a command: ")
+        if prompt == "AVG":
+            c.execute(average)
+            result = c.fetchone()
+            print prompt + ":", result[0]
+        elif prompt == "MAX":
+            c.execute(maximum)
+            result2 = c.fetchone()
+            print prompt + ":", result2[0]
+        elif prompt == "MIN":
+            c.execute(minimum)
+            result3 = c.fetchone()
+            print prompt + ":", result3[0]
+        elif prompt == "SUM":
+            c.execute(sum)
+            result4 = c.fetchone()
+            print prompt + ":", result4[0]
+        elif prompt == "quit":
+            print "You have quit the program."
+            break
+        else:
+            print "Please enter a command: AVG, MAX, MIN, SUM, or quit."
+
+
+
+
+
